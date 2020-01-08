895a43ba70946ce733ed6bc8509d71c8f16473b3
izumi23 <izumiwatanabe23@gmail.com>
2020-01-08 22:12:33 +0100

    wiki imprime les nombres de mots

--- 2_wiki/wiki.c
+++ 2_wiki/wiki.c
@@ -5,47 +5,36 @@
 
 
 int main (int argc, char **argv) {
-  
+
+  if (argc == 1) {
+    puts("Veuillez donner un nombre de pages en argument");
+    return 0;
+  }
+
   int n = atoi(argv[1]);
-  int file_pipe[2];
-  int write_pipe[2];
-  int read_pipe[2];
-  pipe(file_pipe);
-  pipe(write_pipe);
-  pipe(read_pipe);
-  
+  int *pages = malloc(2*n * sizeof(int));
+  int i;
+  for (i=0; i<n; i++) if (pipe(&pages[2*i]) == -1) perror("pipe");;
+
   if (!fork()) {
     int i;
-    char c = 'a';
-    close(file_pipe[0]);
-    close(write_pipe[0]);
-    close(read_pipe[1]);
-    dup2(file_pipe[1], 1);
     for (i=0; i<n; i++) {
-      if (fork()) {
-	if (i>0) write(write_pipe[1], &c, 1);
-	read(read_pipe[0], &c, 1);
-	execlp("curl", "curl", "-s", "http://www.lsv.fr/~hondet/", NULL);
-      }
+      dup2(pages[2*i+1], 1);
+      close(pages[2*i]);
+      if (!fork())
+        execlp("curl", "curl", "-s", "http://www.lsv.fr/~hondet/", NULL);
     }
-    write(write_pipe[1], &c, 1);
   }
-  
+
   else {
     int i;
-    char c;
-    close(file_pipe[1]);
-    close(read_pipe[0]);
-    close(write_pipe[1]);
-    dup2(file_pipe[0], 0);
     for (i=0; i<n; i++) {
-      if (fork()) {
-	write(read_pipe[1], &c, 1);
-	read(write_pipe[0], &c, 1);
-        execlp("wc", "wc", "-w", NULL);
-      }
+      dup2(pages[2*i], 0);
+      close(pages[2*i+1]);
+      if (!fork()) execlp("wc", "wc", "-w", NULL);
     }
-    wait(NULL);
+
+    for (i=0; i<2*n; i++) wait(NULL);
     return 0;
   }
 
